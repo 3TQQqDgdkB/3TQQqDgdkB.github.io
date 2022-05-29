e2c3f0a6a21d191950b4ac899362c202f63be863
3TQQqDgdkB <106409955+3TQQqDgdkB@users.noreply.github.com>
2022-05-29 22:29:25 +0100

    finished files

--- backend.js
+++ backend.js
@@ -17,8 +17,6 @@ const filterData = (inputs) => {
 	const data = compileData(inputs);
 	let correct = 0;
 
-	
-
 	if (currentState === 0) {
 
 		for (let i = 0; i < data.length; i++) {
@@ -57,35 +55,68 @@ const filterData = (inputs) => {
 
 		}
 	}
+	else if (currentState === 2) {
+		
+		for (let i = 0; i < data.length; i++) {
+
+			if (data[i].toLowerCase() === "you") {
+				correct++;
+			}
+			else if (data[i].toLowerCase() === "me") {
+				correct++;
+			}
+			else if (data[i].toLowerCase() === "i") {
+				correct++;
+			}
+			else if (data[i].toLowerCase() === "we") {
+				correct++;
+			}
+			else if (data[i].toLowerCase() === "us") {
+				correct++;
+			}
+			else if (data[i].toLowerCase() === "all of us") {
+				correct++;
+			}
+		}
+
+		if (correct >= 1) {
+
+			localStorage.setItem("state", 758912);
+			location.reload();
+
+		}
+	}
+
 
 }
 
 const createStep1 = () => {
 
-	let text = document.createElement("p")
-	text.innerHTML = "w̷͉̍h̵̭̕a̶̮͝t̶̞́ ̷̱͛ḭ̸͝s̶̒͜ ̶̱̾t̴̪͛h̶̩̆ȅ̵ͅ ̶͉͗c̷̳̏ŏ̵͖r̷̳͐r̵̹̆ȩ̶̄l̵̠͂a̸̝̕t̶̖̀i̵̩̔ŏ̶̱n̷̥̚?̴͠ͅ"
+	let text = document.createElement("p");
+	text.innerHTML = "w̷͉̍h̵̭̕a̶̮͝t̶̞́ ̷̱͛ḭ̸͝s̶̒͜ ̶̱̾t̴̪͛h̶̩̆ȅ̵ͅ ̶͉͗c̷̳̏ŏ̵͖r̷̳͐r̵̹̆ȩ̶̄l̵̠͂a̸̝̕t̶̖̀i̵̩̔ŏ̶̱n̷̥̚?̴͠ͅ";
 
 	let input1 = document.createElement("input");
 	input1.type = "text";
 	
 	let submit = document.createElement("button");
-	submit.innerHTML = "S̴̡̠̓̊̚͜ê̵̜͍͑͐n̵͔̊͋͐ḑ̷͕͐̓"
+	submit.innerHTML = "S̴̡̠̓̊̚͜ê̵̜͍͑͐n̵͔̊͋͐ḑ̷͕͐̓";
 	submit.className = "buttonSubmit";
 
-	document.body.appendChild(text)
+	document.body.appendChild(text);
 	
 	document.body.appendChild(input1);
 	document.body.appendChild(submit);
 
-	submit.addEventListener("click", () => {filterData([input1])})
+	submit.addEventListener("click", () => {filterData([input1])});
 
-};
+}
 
 const createStep2 = () => {
-	
 
-	let text = document.createElement("p")
-	text.innerHTML = "t̷̞̏h̸͚̐ȇ̵̥ ̶̳͗ṱ̵̽h̴̦̐r̸͈̀e̷̐͜e̶̻̋ ̴̲͑s̵͍͂ẗ̴̮́a̸̗͂t̴̥̄ḛ̵͂s̵̩͝,̸͚́ ̷̟̐î̴͇n̵̩͊c̸̖̚ļ̸̔u̸̥̇d̸̬̔i̵̝͑n̵̥̏ģ̴̿ ̵̰̋ţ̵̀h̸̦̔e̵̹̚ ̸͓͌ḿ̶̥ĩ̸͇ŝ̶͉ŝ̴̳ḯ̶̟n̴̼̿g̷̹͑ ̴͖̇l̸͖͋i̶͉̾n̶͓̊k̶̆͜"
+	let text = document.createElement("p");
+	let text2 = document.createElement("p");
+	text.innerHTML = "t̴̩͝h̷́ͅḙ̴̈́ ̸̛̖c̵̪̒ŭ̷͕r̴͈͝r̸̝̓e̷̖͑n̸͖̒t̵̐ͅ ̸͓̅a̷̦̓n̵͖̔d̴̟͝ ̴͙̀p̵͖͠r̸͉̋ê̴͜v̸̮̂î̷̼ŏ̷̰u̷͚͊s̴̯̉ ̶̻̒a̶̻͠t̸̼̒t̴͖̕e̷̺̔m̶̞͗p̵͖̅t̴̩̒s̵̹̓";
+	text2.innerHTML = "a̷̟͊n̴̰͠d̴̡͝ ̶͓̓t̷̫͠h̵̬̀ḛ̷̀ ██████ ą̷̓t̴̞̽t̷͉͑ë̸̞́m̵̮̑p̶̳̃ț̸͒";
 
 	let input1 = document.createElement("input");
 	input1.type = "text";
@@ -95,8 +126,11 @@ const createStep2 = () => {
 	input3.type = "text";
 	
 	let submit = document.createElement("button");
-	submit.innerHTML = "S̴̡̠̓̊̚͜ê̵̜͍͑͐n̵͔̊͋͐ḑ̷͕͐̓"
+	submit.innerHTML = "S̴̡̠̓̊̚͜ê̵̜͍͑͐n̵͔̊͋͐ḑ̷͕͐̓";
 	submit.className = "buttonSubmit";
+
+	document.body.appendChild(text);
+	document.body.appendChild(text2);
 	
 	document.body.appendChild(input1);
 	document.body.appendChild(input2);
@@ -104,13 +138,47 @@ const createStep2 = () => {
 
 	document.body.appendChild(submit);
 
-	submit.addEventListener("click", () => {filterData([input1, input2, input3])})
+	submit.addEventListener("click", () => {filterData([input1, input2, input3])});
 
-};
+}
+
+const createStep3 = () => {
+
+	let text = document.createElement("p");
+	text.innerHTML = 'w̵̫̌h̶̻́o̴͎͗ ̷̠̓à̶̩r̶̰̅ê̸̫ ̶̪͠"̴͔͒t̷͔́h̵̡͒e̴̠͂y̶͙͋"̶͇͊';
+
+	let input1 = document.createElement("input");
+	input1.type = "text";
+	
+	let submit = document.createElement("button");
+	submit.innerHTML = "S̴̡̠̓̊̚͜ê̵̜͍͑͐n̵͔̊͋͐ḑ̷͕͐̓";
+	submit.className = "buttonSubmit";
+
+	document.body.appendChild(text);
+	
+	document.body.appendChild(input1);
+
+	document.body.appendChild(submit);
+
+	submit.addEventListener("click", () => {filterData([input1])});
+
+}
+
+const showVid1 = () => {
+
+	let text = document.createElement("p");
+	text.innerHTML = "https://youtu.be/IZIg9mMneqA";
+
+	document.body.appendChild(text);
+
+}
 
 const state = localStorage.getItem("state");
 if (state === "146526") { currentState = 1; }
 else if (state === "481495") { currentState = 2; }
+else if (state === "758912") { currentState = 3; }
 
-if (currentState === 0) { createStep1() }
-else if (currentState === 1) { createStep2() }
+if (currentState === 0) { createStep1(); }
+else if (currentState === 1) { createStep2(); }
+else if (currentState === 2) { createStep3(); }
+else if (currentState === 3) { showVid1(); }
